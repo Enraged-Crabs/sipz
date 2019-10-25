cd71d5f39127635d1cac50637deb38d2c395d40e
Ciara Anderson <thelaylinguist@gmail.com>
2019-10-25 01:02:10 +0000

    Updated user fields

--- db/migrate/20191024164406_add_fields_to_user_table.rb
+++ db/migrate/20191024164406_add_fields_to_user_table.rb
@@ -2,7 +2,8 @@ class AddFieldsToUserTable < ActiveRecord::Migration[5.2]
   def change
 
     add_column :users, :moderator, :boolean
-    add_column :users, :brewery, :boolean
+    add_column :users, :brewery, :boolean, default: false
     add_column :users, :location, :string
+    add_column :users, :username, :string
   end
 end
