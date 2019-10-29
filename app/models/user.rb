b24559caf4d914546d4dc31360c6bb9bd06136c9
rayazanderson <rayazand@gmail.com>
2019-10-29 01:07:36 +0000

    added usernames, but incomplete. 5 faliures in rspec (post screenshot on slack for help)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,20 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  attr_writer :login
 
   def login
     @login || self.username || self.email
   end
+
+# getter
+# def login
+# @login
+# end
+
+# setter
+
+# def login =(str)
+# @login =str
+# end
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -17,13 +25,16 @@ class User < ApplicationRecord
   
   validates :username, presence: :true, uniqueness: { case_sensitive: false }
 
-    def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      if login = conditions.delete(:login)
-        where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { 
-          :value => login.downcase }]).first
-      elsif conditions.has_key?(:username) || conditions.has_key?(:email)
-        where(conditions.to_h).first
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
       end
     end
+  end
 end
