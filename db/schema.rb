24c5e080d82f8326fea0f44aef72e285d459b2e3
Ciara Anderson <thelaylinguist@gmail.com>
2019-11-07 22:41:13 +0000

    Can now add post pics

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_07_192547) do
+ActiveRecord::Schema.define(version: 2019_11_07_222822) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_11_07_192547) do
     t.integer "beer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
     t.index ["beer_id"], name: "index_posts_on_beer_id"
     t.index ["user_id", "beer_id"], name: "index_posts_on_user_id_and_beer_id"
   end
